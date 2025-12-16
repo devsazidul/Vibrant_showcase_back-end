2d2d90caf62e455e4bad7f2b22c0e7cc7e8b6684
devsazidul <rabbiking00@gmail.com>
2025-12-16 17:16:43 +0600

    test done

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from . import authentication
 
 urlpatterns = [
     path('test/', views.test_api, name='test'),
     path('hello/', views.hello, name='hello'),
+    path('test1/', authentication.testapi_2, name='test1'),
 ]
